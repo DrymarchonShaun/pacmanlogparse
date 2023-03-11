d9726f6db6087775c5fd74f717179e3e19e032e9
drymarchonshaun <draedena.miller@gmail.com>
2023-03-11 09:58:55 -0800

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,39 @@
-use std::fs;
-use std::vec::Vec;
 use regex::Regex;
+use std::fs;
+use std::ops::Add;
+use std::vec::Vec;
 
 
 fn main() {
-    let file_path = "/var/log/pacman.log";
+    let file_path = String::from("/var/log/pacman.log");
 
+    let base_log = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
-    let base_log = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
+    let mut operation: Vec<String> = Vec::new();
+    let mut package: Vec<String> = Vec::new();
 
-    let mut installed: Vec<String> = Vec::new();
-    let mut reinstalled: Vec<String> = Vec::new();
-    let mut removed: Vec<String> = Vec::new();
+    for cap in Regex::new(r"\[\d{4}\-\d{2}\-\d{2}T\d{2}:\d{2}:\d{2}(?:\+|\-)\d{4}\] \[ALPM\] (installed|removed) (\S+\w)").unwrap().captures_iter(&base_log) {
+        operation.push(cap[1].to_string());
+        package.push(cap[2].to_string());
+    }
 
-    for cap in Regex::new(r"\[\d{4}\-\d{2}\-\d{2}T\d{2}:\d{2}:\d{2}(?:\+|\-)\d{4}\] \[ALPM\] (installed|reinstalled|removed) (\S+\w)").unwrap().captures_iter(&base_log) {
-        if cap[1].to_string() == "installed" {
-            installed.push(cap[2].to_string());
-        } else if cap[1].to_string() == "reinstalled" {
-                reinstalled.push(cap[2].to_string()); 
-            } else {
-                removed.push(cap[2].to_string());
-                };
-        };
-    println!("{:?}", installed);
-    println!("{:?}", reinstalled);
-    println!("{:?}", removed);
-}
+    fn pkg_mgr(operation: String, package: String) {
+        let mut to_install: Vec<String> = Vec::new();
+        if operation == "installed" {
+            to_install.push(package);
+        } else if operation == "removed" {
+            let checkpkg = to_install.iter().position(|x| x == &package);
+            if checkpkg != None {
+                to_install.remove(checkpkg.unwrap());
+            };
+            };
+        }
+    if package.len() == operation.len() {
+        let mut i = 0;
+        while i < package.len() {
+         packagelist = pkg_mgr(operation[i].to_string(), package[i].to_string());
+            i += 1;
+        }
+        
+    }
+    }
