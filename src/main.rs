8b74d3c2614f831c7a00e2e691a28b6ae6389234
drymarchonshaun <draedena.miller@gmail.com>
2023-03-24 12:51:24 -0700

    First Commit

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,35 @@
 use regex::Regex;
 use std::fs;
-use std::ops::Add;
 use std::vec::Vec;
 
-
 fn main() {
-    let file_path = String::from("/var/log/pacman.log");
+    let log_path = String::from("/var/log/pacman.log");
+    let file_path = String::from("./package_list");
 
-    let base_log = fs::read_to_string(file_path).expect("Should have been able to read the file");
-
+    let base_log = fs::read_to_string(log_path).expect("Should have been able to read the file");
     let mut operation: Vec<String> = Vec::new();
     let mut package: Vec<String> = Vec::new();
+    let mut package_list: Vec<String> = Vec::new();
 
-    for cap in Regex::new(r"\[\d{4}\-\d{2}\-\d{2}T\d{2}:\d{2}:\d{2}(?:\+|\-)\d{4}\] \[ALPM\] (installed|removed) (\S+\w)").unwrap().captures_iter(&base_log) {
+    for cap in Regex::new(r"\[\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}[+\-]\d{4}] \[ALPM] (installed|removed) (\S+\w)").unwrap().captures_iter(&base_log) {
         operation.push(cap[1].to_string());
         package.push(cap[2].to_string());
     }
 
-    fn pkg_mgr(operation: String, package: String) {
-        let mut to_install: Vec<String> = Vec::new();
-        if operation == "installed" {
-            to_install.push(package);
-        } else if operation == "removed" {
-            let checkpkg = to_install.iter().position(|x| x == &package);
-            if checkpkg != None {
-                to_install.remove(checkpkg.unwrap());
-            };
-            };
-        }
     if package.len() == operation.len() {
         let mut i = 0;
-        while i < package.len() {
-         packagelist = pkg_mgr(operation[i].to_string(), package[i].to_string());
+        for entry in package {
+            if operation.get(i) == Some(&"installed".to_string()) {
+                package_list.push(entry);
+            } else if package_list.iter().position(|x| x == &entry) != None {
+                package_list.remove(package_list.iter().position(|x| x == &entry).unwrap());
+            };
             i += 1;
         }
-        
+        fs::write(
+            file_path,
+            format!("paru -S --noconfirm {}", package_list.join(" ")),
+        )
+        .expect("");
     }
-    }
+}
